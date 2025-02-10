8916083d017f5b9097383a4d23397ad1884e952b
bxbx4mzz <beam.wipawee2548@gmail.com>
2025-02-10 16:08:30 +0700

    gameplay

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("Flash Card");  // Set the new title
+    this->setFixedSize(800, 600);  // set dufaulf window to 800*600
 }
 
 MainWindow::~MainWindow()
