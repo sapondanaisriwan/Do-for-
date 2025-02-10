82c3536056d4d1268fdb74bfbaaefaa33de0fea0
bxbx4mzz <beam.wipawee2548@gmail.com>
2025-02-10 23:39:26 +0700

    edit picture

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,19 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <QApplication>
-#include <QMainWindow>
-#include <QIcon>
+#include <QIcon> // for icon
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowTitle("Flash Card");  // Set title
+    this->setWindowTitle("Flash Card");  // set title
     this->setFixedSize(800, 600);  // set dufaulf window to 800*600
     this->setWindowIcon(QIcon(":/icon/icon.png")); // set icon from image in qt resources file
 
+    ui->picture_display->setScaledContents(true); // fill picture
+    ui->picture_display->setPixmap(QPixmap(":/icon/icon.png")); // insert picture in label from resource
 }
 
 MainWindow::~MainWindow()
