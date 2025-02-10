d952c1b70b6f88563b5c749435b88a280c84dcb9
bxbx4mzz <beam.wipawee2548@gmail.com>
2025-02-10 16:49:31 +0700

    icon

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QApplication>
+#include <QMainWindow>
+#include <QIcon>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowTitle("Flash Card");  // Set the new title
+    this->setWindowTitle("Flash Card");  // Set title
     this->setFixedSize(800, 600);  // set dufaulf window to 800*600
+    this->setWindowIcon(QIcon("D:/c.prog/Die-for-you/icon.png")); //set icon
 }
 
 MainWindow::~MainWindow()
